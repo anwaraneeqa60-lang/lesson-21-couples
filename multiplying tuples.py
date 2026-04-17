f9e5c50a50313e6580125f92723a88f51fe59671
anwaraneeqa60-lang <anwaraneeqa60@gmail.com>
2026-04-17 12:05:20 -0400

    multiplying tuples.py

---
+++ multiplying tuples.py
@@ -0,0 +1,8 @@
+import math
+nums = (4, 3, 2, 2, -1, 18)
+product = math.prod(nums)
+print(f"The product of the tuple {nums} is: {product}")
+import math
+nums = (2, 4, 8, 8, 3, 2, 9)
+product = math.prod(nums)
+print(f"The product of the tuple {nums} is: {product}")
